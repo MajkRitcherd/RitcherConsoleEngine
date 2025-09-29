58e3738d67c38843974e7d44a28c73fe9a9385a0
MajkRitcherd <misutak87@gmail.com>
2025-09-29 15:10:54 +0200

    Add Console's COORD and SMALL_RECT structures

---
+++ RitcherConsoleEngine/WinAPI/ConsoleApiStructures/ConsoleCoordinate.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+
+namespace RitcherConsoleEngine.WinAPI.ConsoleApiStructures
+{
+    /// <summary>
+    /// Defines the coordinates of a character cell in a console screen buffer. <br />
+    /// See https://learn.microsoft.com/en-us/windows/console/coord-str.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct ConsoleCoordinate
+    {
+        /// <summary>
+        /// The horizontal coordinate or column value.
+        /// </summary>
+        public short X;
+
+        /// <summary>
+        /// The vertical coordinate or row value.
+        /// </summary>
+        public short Y;
+    }
+}
---
+++ RitcherConsoleEngine/WinAPI/ConsoleApiStructures/ConsoleSmallRectangle.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.InteropServices;
+
+namespace RitcherConsoleEngine.WinAPI.ConsoleApiStructures
+{
+    /// <summary>
+    /// Defines the coordinates of the upper left and lower right corners of a rectangle. <br />
+    /// See https://learn.microsoft.com/en-us/windows/console/small-rect-str.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct ConsoleSmallRectangle
+    {
+        /// <summary>
+        /// The X-coordinate of the upper left corner of the rectangle.
+        /// </summary>
+        public short Left;
+
+        /// <summary>
+        /// The Y-coordinate of the upper left corner of the rectangle.
+        /// </summary>
+        public short Top;
+
+        /// <summary>
+        /// The X-coordinate of the lower right corner of the rectangle.
+        /// </summary>
+        public short Right;
+
+        /// <summary>
+        /// The Y-coordinate of the lower right corner of the rectangle.
+        /// </summary>
+        public short Bottom;
+    }
+}
