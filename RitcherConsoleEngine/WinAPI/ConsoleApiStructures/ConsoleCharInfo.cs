4732a81c24922fa3355d37030cefdc56ba677033
MajkRitcherd <misutak87@gmail.com>
2025-09-30 09:13:35 +0200

    Add Char info structure to match CHAR_INFO

---
+++ RitcherConsoleEngine/WinAPI/ConsoleApiStructures/ConsoleCharInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+
+namespace RitcherConsoleEngine.WinAPI.ConsoleApiStructures
+{
+    /// <summary>
+    /// Specifies a Unicode or ANSI character and its attributes. <br />
+    /// See https://learn.microsoft.com/en-us/windows/console/char-info-str.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    internal struct ConsoleCharInfo
+    {
+        /// <summary>
+        /// Internal representation of the Char. <br />
+        /// Represented as ushort because of bite match with WCHAR (https://learn.microsoft.com/en-us/windows/win32/extensible-storage-engine/wchar).
+        /// </summary>
+        [FieldOffset(0)]
+        private ushort _internalCharacterRepresentation;
+
+        /// <summary>
+        /// The character attributes. This member can be zero or any combination of the following values.
+        /// </summary>
+        [FieldOffset(2)]
+        public short Attributes;
+
+        /// <summary>
+        /// Unicode character of a screen buffer character cell.
+        /// </summary>
+        public char Character
+        {
+            readonly get => (char)_internalCharacterRepresentation;
+            set => _internalCharacterRepresentation = value;
+        }
+    }
+}
