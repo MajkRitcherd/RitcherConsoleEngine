dced5b516eb412373b7b5dd2141b3a3670fbc740
MajkRitcherd <misutak87@gmail.com>
2025-10-01 08:24:26 +0200

    Add console core exception

---
+++ RitcherConsoleEngine/ConsoleCoreException.cs
@@ -0,0 +1,35 @@
+﻿namespace RitcherConsoleEngine
+{
+    /// <summary>
+    /// Console CORE exception.
+    /// </summary>
+    internal class ConsoleCoreException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConsoleCoreException"/> class.
+        /// </summary>
+        public ConsoleCoreException()
+            : base()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConsoleCoreException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public ConsoleCoreException(string? message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConsoleCoreException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
+        public ConsoleCoreException(string? message, Exception? innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
