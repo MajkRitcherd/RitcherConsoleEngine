7b40a86e0fa52e4929aa41bf5818578ffab7fe3d
MajkRitcherd <misutak87@gmail.com>
2025-10-03 10:23:23 +0200

    Add another thread that'll run the game loop, move console creation from constructor to own method, called on Run() method

--- Examples/TestRCE/Program.cs
+++ Examples/TestRCE/Program.cs
@@ -2,7 +2,8 @@
 
 using (var game = new TestGame(150, 50))
 {
-}
+    game.Run();
 
-// Wait so we can see the console
-Console.ReadKey();
+    // Wait so we can see the console
+    Console.ReadKey();
+}
--- RitcherConsoleEngine/RitcherConsoleGame.cs
+++ RitcherConsoleEngine/RitcherConsoleGame.cs
@@ -13,8 +13,10 @@ namespace RitcherConsoleEngine
     /// </remarks>
     public abstract class RitcherConsoleGame : IDisposable
     {
+        private readonly Thread _gameThread;
         private bool _disposed;
         private IntPtr _screenBufferHandle;
+        private ConsoleCoordinate _screenBufferSize;
         private short _screenHeight;
         private short _screenWidth;
         private ConsoleSmallRectangle _windowCoordinates;
@@ -29,12 +31,9 @@ namespace RitcherConsoleEngine
             ScreenHeight = screenHeight;
             ScreenWidth = screenWidth;
 
+            _gameThread = new Thread(GameThreadLoop);
+            _screenBufferSize = new ConsoleCoordinate { X = ScreenWidth, Y = ScreenHeight };
             _windowCoordinates = new ConsoleSmallRectangle { Left = 0, Top = 0, Right = (short)(ScreenWidth - 1), Bottom = (short)(ScreenHeight - 1) };
-            _screenBufferHandle = WinConsoleAPI.CreateScreenBuffer(AccessRights.GenericReadAndWrite, ShareModes.FileShareReadAndWrite);
-
-            WinConsoleAPI.SetActiveScreenBuffer(_screenBufferHandle);
-            WinConsoleAPI.SetScreenBufferSize(_screenBufferHandle, new ConsoleCoordinate { X = ScreenWidth, Y = ScreenHeight });
-            WinConsoleAPI.SetWindowInfo(_screenBufferHandle, true, ref _windowCoordinates);
         }
 
         /// <summary>
@@ -75,6 +74,18 @@ namespace RitcherConsoleEngine
         }
 
         /// <summary>
+        /// Runs the application.
+        /// </summary>
+        public void Run()
+        {
+            if (!CreateConsole())
+                throw new ConsoleCoreException("Failed to create console");
+
+            _gameThread.Start();
+            _gameThread.Join();
+        }
+
+        /// <summary>
         /// Disposes managed and unmanaged resources.
         /// </summary>
         /// <param name="disposing">Whether or not it is called manually or by GC.</param>
@@ -93,6 +104,49 @@ namespace RitcherConsoleEngine
                 WinConsoleAPI.CloseHandle(_screenBufferHandle);
 
             _disposed = true;
+        }
+
+        /// <summary>
+        /// Creates the console using WinAPI calls.
+        /// </summary>
+        /// <remarks>
+        /// Gets the standard output handle and sets its window size to the smallest one,
+        /// so we can create window of any size. <br />
+        /// Then it creates screen buffer (double buffering) and sets it as active screen buffer
+        /// and sets the window size.
+        /// </remarks>
+        /// <returns>True, if creating console was successful, otherwise false.</returns>
+        private bool CreateConsole()
+        {
+            var smallRect = new ConsoleSmallRectangle
+            {
+                Left = 0,
+                Top = 0,
+                Right = 0,
+                Bottom = 0
+            };
+
+            // Set output console to small window so we can set any window size later on
+            var handleConsole = WinConsoleAPI.GetStdHandle(StdHandleTypes.OutputHandle);
+            WinConsoleAPI.SetWindowInfo(handleConsole, true, ref smallRect);
+
+            // Prepare console buffer/size
+            _screenBufferHandle = WinConsoleAPI.CreateScreenBuffer(AccessRights.GenericReadAndWrite, ShareModes.FileShareReadAndWrite);
+            WinConsoleAPI.SetActiveScreenBuffer(_screenBufferHandle);
+            WinConsoleAPI.SetScreenBufferSize(_screenBufferHandle, _screenBufferSize);
+            WinConsoleAPI.SetWindowInfo(_screenBufferHandle, true, ref _windowCoordinates);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Method executed by thread representing the main loop of an application.
+        /// </summary>
+        private void GameThreadLoop()
+        {
+            while (true)
+            {
+            }
         }
     }
 }
